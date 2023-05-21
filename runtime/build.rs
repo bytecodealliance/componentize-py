bfd93fd5c03f5150682b4c26ef0dc8c3f211c11b
Joel Dice <joel.dice@fermyon.com>
2023-05-21 15:25:09 -0600

    downgrade to Wasmtime 8.0.0
    It seems that Wasmtime 8.0.1 introduced a WASI filesystem regression on Windows
    which prevents CPython from running.  I'm still tracking it down, but for the
    time being downgrading is the only option.
    
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- runtime/build.rs
+++ runtime/build.rs
@@ -10,6 +10,7 @@ fn main() {
     let wasi_sdk_path =
         std::env::var("WASI_SDK_PATH").unwrap_or_else(|_| "/opt/wasi-sdk".to_owned());
 
+    println!("cargo:rerun-if-env-changed=WASI_SDK_PATH");
     println!("cargo:rustc-link-search={wasi_sdk_path}/share/wasi-sysroot/lib/wasm32-wasi");
     println!("cargo:rustc-link-lib=wasi-emulated-signal");
     println!("cargo:rustc-link-lib=wasi-emulated-getpid");
