695bd29c5ea2284ef1c07c6991ae44e7eff82e33
Joel Dice <joel.dice@fermyon.com>
2023-08-25 08:31:58 -0600

    add comments and docs to `http` example
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- examples/http/app.py
+++ examples/http/app.py
@@ -1,3 +1,19 @@
+"""Demo of a serverless app using `wasi-http` to handle inbound HTTP requests.
+
+This demonstrates how to use WASI's asynchronous capabilities to manage multiple
+concurrent requests and streaming bodies.  It uses a custom `asyncio` event loop
+to thread I/O through coroutines.
+"""
+
+# Note that we've temporarily renamed various `wasi-http` and `wasi-cli`
+# interfaces (appending a 2 to each name) to avoid conflicting with the
+# implementations in `wasmtime-wasi` which are still under development.
+#
+# Also note that `wasi-http` currently uses pseudo-resources (represented as
+# integers) to model requests, responses, etc.  As of this writing, proper WIT
+# resource support is still under development; we'll update this example to use
+# them once they're ready.
+
 import asyncio
 import hashlib
 import poll_loop
@@ -11,17 +27,32 @@ from typing import Tuple, cast
 from urllib import parse
 
 class IncomingHandler2(exports.IncomingHandler2):
+    """Implements the `export`ed portion of the `wasi-http` `proxy` world."""
+    
     def handle(self, request: int, response_out: int):
+        """Handle the specified `request` (represented as a pseudo-resource), sending
+        the response to `response_out`.
+        """
+        # Dispatch the request using `asyncio`, backed by a custom event loop
+        # based on WASI's `poll_oneoff` function.
         loop = PollLoop()
         asyncio.set_event_loop(loop)
         loop.run_until_complete(handle_async(request, response_out))
 
 async def handle_async(request: int, response_out: int):
+    """Handle the specified `request` (represented as a pseudo-resource), sending
+    the response to `response_out`."""
+    
     method = types.incoming_request_method(request)
     path = types.incoming_request_path_with_query(request)
     headers = types.fields_entries(types.incoming_request_headers(request))
 
     if isinstance(method, MethodGet) and path == "/hash-all":
+        # Collect one or more "url" headers, download their contents
+        # concurrently, compute their SHA-256 hashes incrementally (i.e. without
+        # buffering the response bodies), and stream the results back to the
+        # client as they become available.
+        
         urls = map(lambda pair: str(pair[1], "utf-8"), filter(lambda pair: pair[0] == "url", headers))
 
         response = types.new_outgoing_response(200, types.new_fields([("content-type", b"text/plain")]))
@@ -36,6 +67,8 @@ async def handle_async(request: int, response_out: int):
 
         sink.close()
     elif isinstance(method, MethodPost) and path == "/echo":
+        # Echo the request body back to the client without buffering.
+        
         response = types.new_outgoing_response(
             200,
             types.new_fields(list(filter(lambda pair: pair[0] == "content-type", headers)))
@@ -59,6 +92,12 @@ async def handle_async(request: int, response_out: int):
         types.finish_outgoing_stream(types.outgoing_response_write(response))
 
 async def sha256(url: str) -> Tuple[str, str]:
+    """Download the contents of the specified URL, computing the SHA-256
+    incrementally as the response body arrives.
+
+    This returns a tuple of the original URL and the hex-encoded hash.
+    """
+    
     url_parsed = parse.urlparse(url)
 
     match url_parsed.scheme:
@@ -96,6 +135,8 @@ async def sha256(url: str) -> Tuple[str, str]:
             hasher.update(chunk)
 
 async def outgoing_request_send(request: int) -> int:
+    """Send the specified request and wait asynchronously for the response."""
+    
     future = outgoing_handler.handle(request, None)
     pollable = types.listen_to_future_incoming_response(future)
 
