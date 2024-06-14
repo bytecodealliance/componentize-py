634a47017deed3a06c4ae7505f4d66961148482c
Joel Dice <joel.dice@fermyon.com>
2024-06-14 09:51:11 -0600

    bump version to 0.13.4
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- src/lib.rs
+++ src/lib.rs
@@ -462,7 +462,7 @@ pub async fn componentize(
                 .unwrap()
                 .to_str()
                 .context("non-UTF-8 path")?
-		.replace('\\', "/");
+                .replace('\\', "/");
 
             libraries.push(Library {
                 name: format!("/{index}/{path}"),
@@ -916,9 +916,9 @@ fn search_directory(
                     // subdirectory to be the true owner of the file.  This is important later, when we derive a
                     // package name by stripping the root directory from the file path.
                     if root > existing.root {
-                        existing.module = module.clone();
-                        existing.root = root.to_owned();
-                        existing.path = path.parent().unwrap().to_owned();
+                        module.clone_into(&mut existing.module);
+                        root.clone_into(&mut existing.root);
+                        path.parent().unwrap().clone_into(&mut existing.path);
                     }
                     push = false;
                     break;
