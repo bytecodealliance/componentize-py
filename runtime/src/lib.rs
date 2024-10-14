dc61f34940465e3b60b350d0bc21f71906d949d8
Joel Dice <joel.dice@fermyon.com>
2024-10-14 09:17:04 -0600

    update to wit-bindgen 0.34.0
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -28,10 +28,10 @@ use {
 wit_bindgen::generate!({
     world: "init",
     path: "../wit",
-    exports: {
-        "exports": MyExports
-    }
+    generate_all,
 });
+
+export!(MyExports);
 
 static STUB_WASI: OnceCell<bool> = OnceCell::new();
 static EXPORTS: OnceCell<Vec<Export>> = OnceCell::new();
@@ -1281,3 +1281,21 @@ pub mod dl {
 static _CLOCK_PROCESS_CPUTIME_ID: u8 = 2;
 #[no_mangle]
 static _CLOCK_THREAD_CPUTIME_ID: u8 = 3;
+
+// Traditionally, `wit-bindgen` would provide a `cabi_realloc` implementation, but recent versions use a weak
+// symbol trick to avoid conflicts when more than one `wit-bindgen` version is used, and that trick does not
+// currently play nice with how we build this library.  So for now, we just define it ourselves here:
+/// # Safety
+/// TODO
+#[export_name = "cabi_realloc"]
+pub unsafe extern "C" fn cabi_realloc(
+    old_ptr: *mut u8,
+    old_len: usize,
+    align: usize,
+    new_size: usize,
+) -> *mut u8 {
+    assert!(old_ptr.is_null());
+    assert!(old_len == 0);
+
+    alloc::alloc(Layout::from_size_align(new_size, align).unwrap())
+}
