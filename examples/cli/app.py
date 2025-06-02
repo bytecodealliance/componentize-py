a1cc87f58f1725c67cb14725e1e840a90efb254f
Joel Dice <joel.dice@fermyon.com>
2025-06-02 10:53:42 -0600

    remove pdb code from CLI example
    I hadn't meant to check that in earlier; it was intended only for a local
    experiment.
    
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- examples/cli/app.py
+++ examples/cli/app.py
@@ -1,9 +1,5 @@
 from wit_world import exports
-from wit_world.imports.environment import get_arguments
-import pdb
 
 class Run(exports.Run):
     def run(self) -> None:
-        if "--pdb" in get_arguments():
-            pdb.set_trace()
         print("Hello, world!")
